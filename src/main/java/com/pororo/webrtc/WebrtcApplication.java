6d51fa3f8bda08855e1001b4a379f043c08a2796
SKrns <rnsk3626@gmail.com>
2020-05-19 12:09:13 +0900

    Initial Commit, Basic WebRTC
    Created signaling Server.
    Created simple chat client.

---
+++ src/main/java/com/pororo/webrtc/WebrtcApplication.java
@@ -0,0 +1,18 @@
+package com.pororo.webrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrtcApplication {
+	// TODO : 웹캠, 마이크 권한 받고 그리기(navigator.webkitGetUserMedia) - client.js, index.html
+	// TODO : RTC 통해 전송하면서 받기(Stun Server) - client.js
+
+	//https://webrtc.github.io/samples/
+	//https://www.tutorialspoint.com/webrtc/index.htm
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrtcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pororo/webrtc/WebrtcSocketConfig.java
@@ -0,0 +1,18 @@
+package com.pororo.webrtc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class WebrtcSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(new SocketHandler(),"/socket")
+                .setAllowedOrigins("*");
+    }
+}
